0af5a536705391c601b66ebc3a3d1a0baf97f8ac
Oleh <zaaqq135@gmail.com>
2024-06-28 17:31:35 +0300

    set events

--- app/Livewire/Polls.php
+++ app/Livewire/Polls.php
@@ -4,12 +4,16 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Poll;
+use Livewire\Attributes\On;
 
 class Polls extends Component
 {
+    #[On('pollCreated')]
     public function render()
     {
         $polls = Poll::with('options.votes')->latest()->get();
         return view('livewire.polls', ['polls' => $polls]);
     }
+
+    
 }
